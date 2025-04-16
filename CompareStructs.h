79a54c8166da2a39d789869b3c5f41655bf3a2c0
Youssef Okeil <youssef_okeil@yahoo.com>
2025-04-17 01:22:49 +0200

    Refactor Structs in header file

---
+++ CompareStructs.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "process_class.h"
+
+struct CompareByBurstTime {
+    bool operator()(const Process& a, const Process& b) {
+        if (a.getBurstTime() == b.getBurstTime())
+        {
+            if (a.getArrivalTime() == b.getArrivalTime())
+                return a.getID() > b.getID();
+
+            return a.getArrivalTime() > b.getArrivalTime();
+
+        }
+        return a.getBurstTime() > b.getBurstTime();
+
+    }
+};
+struct CompareByRemainingTime {
+    bool operator()(const Process& a, const Process& b) {
+        if (a.getRemainingTime() == b.getRemainingTime())
+        {
+            if (a.getArrivalTime() == b.getArrivalTime())
+                return a.getID() > b.getID();
+
+            return a.getArrivalTime() > b.getArrivalTime();
+
+        }
+        return a.getRemainingTime() > b.getRemainingTime();
+
+    }
+};
+
+struct CompareByArrival {
+    bool operator()(const Process& a, const Process& b) {
+        if (a.getArrivalTime() == b.getArrivalTime())
+        {
+            if (a.getRemainingTime() == b.getRemainingTime())
+                return a.getID() > b.getID();
+
+            return a.getRemainingTime() > b.getRemainingTime();
+
+        }
+        return a.getArrivalTime() > b.getArrivalTime();
+
+    }
+};
+
+struct CompareByPriority {
+    bool operator()(const Process& a, const Process& b) {
+        return a.get_priority() > b.get_priority();
+
+    }
+};
