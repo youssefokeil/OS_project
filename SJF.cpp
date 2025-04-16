bc298d8bda5accc1be2d1ed9d11f3077b973154a
Youssef Okeil <youssef_okeil@yahoo.com>
2025-04-16 18:10:51 +0200

    Merge Changes of SJF

--- SJF.cpp
+++ SJF.cpp
@@ -12,11 +12,31 @@
 
 using namespace std;
 
-struct CompareByBurst{
+struct CompareByBurstTime{
     bool operator()(const Process &a, const Process &b){
         if(a.getBurstTime()==b.getBurstTime())
+            {
+           if(a.getArrivalTime()==b.getArrivalTime())
+                        return a.getID() >b.getID() ;
+
             return a.getArrivalTime() >b.getArrivalTime();
+
+               }
         return a.getBurstTime() > b.getBurstTime();
+
+    }
+};
+struct CompareByRemainingTime{
+    bool operator()(const Process &a, const Process &b){
+        if(a.getRemainingTime()==b.getRemainingTime())
+            {
+           if(a.getArrivalTime()==b.getArrivalTime())
+                        return a.getID() >b.getID() ;
+
+            return a.getArrivalTime() >b.getArrivalTime();
+
+               }
+        return a.getRemainingTime() > b.getRemainingTime();
 
     }
 };
@@ -24,10 +44,15 @@ struct CompareByBurst{
 struct CompareByArrival{
         bool operator()(const Process &a, const Process &b){
             if(a.getArrivalTime()==b.getArrivalTime())
-                return a.getBurstTime() >b.getBurstTime();
-    
+               {
+           if(a.getRemainingTime()==b.getRemainingTime())
+                        return a.getID() >b.getID() ;
+
+                return a.getRemainingTime() >b.getRemainingTime();
+
+               }
             return a.getArrivalTime() > b.getArrivalTime();
-    
+
         }
 };
 
@@ -37,13 +62,15 @@ int main()
 
     //  min heaps initialization, one sorted by arrival time, and one sorted by burst time.
     priority_queue<Process, std::vector<Process>, CompareByArrival> ProcessesByArrival;
-    priority_queue<Process, std::vector<Process>, CompareByBurst> ProcessesByBurst;
+    priority_queue<Process, std::vector<Process>, CompareByRemainingTime> ProcessesByRemainingTime; //preemptive
+    priority_queue<Process, std::vector<Process>, CompareByBurstTime> ProcessesByBurstTime; //for nonpreemptive
+
     vector<string> gantt_chart;
 
     cout<<"Number of Processes:"<<flush;
     int n;
     cin>>n;
-
+bool preemptive=1;
 
     for(int i=1; i<=n; i++){
             int art=0,bt;
@@ -51,72 +78,82 @@ int main()
             cin>>art;
             cout<<"Burst time:";
             cin>>bt;
-            
+
             Process  p(i,art,bt);
             ProcessesByArrival.push(p);
-            cout<<p.getID()<<" "<<p.getArrivalTime()<<" "<<p.getBurstTime()<<endl;
+            cout<<p.getID()<<" "<<p.getArrivalTime()<<" "<<p.getRemainingTime()<<endl;
 
 
     }
 
     int counter=0;
-    while(!ProcessesByArrival.empty()||!ProcessesByBurst.empty()){
+    while(!ProcessesByArrival.empty()||!ProcessesByRemainingTime.empty()){
+
+
+            if(preemptive==0){
+
+            }
+
+
+
+
+
+
         Process newProcess;
 
-                   
+
         while(!ProcessesByArrival.empty()){
             // top of arrived processes
             newProcess=ProcessesByArrival.top();
             if(newProcess.getArrivalTime()<=counter){
                     // push arrived process
-                ProcessesByBurst.push(newProcess);
-                // update buffer 
+                ProcessesByRemainingTime.push(newProcess);
+                // update buffer
                 ProcessesByArrival.pop();
             }else
                 break;
         }
-        
+
         auto start = chrono::high_resolution_clock::now();
-        
-    
+
+
         cout<< "\ncounter : " << counter<<endl;
-        
+
         Process currentProcess;
-        if(!ProcessesByBurst.empty())
+        if(!ProcessesByRemainingTime.empty())
         // current process from burst time
-            currentProcess=ProcessesByBurst.top();
+            currentProcess=ProcessesByRemainingTime.top();
         else
             currentProcess=Process(0,0,0);
-        
+
         this_thread::sleep_for(chrono::milliseconds(1000));
 
         // update burst time
         if(currentProcess.getRemainingTime()){
-            
 
-          
+
+
             currentProcess.updateRemainingTime(1);
 
-            // print current element       
+            // print current element
             gantt_chart.push_back("P"+to_string(currentProcess.getID())+ " | " );
 
             // update top element
-            ProcessesByBurst.pop();
-            ProcessesByBurst.push(currentProcess);
+            ProcessesByRemainingTime.pop();
+            if(currentProcess.getRemainingTime())
+            ProcessesByRemainingTime.push(currentProcess);
 
-        }
-        else if(!ProcessesByBurst.empty())
-            ProcessesByBurst.pop();
+           }
         else
             gantt_chart.push_back("__ | ");
 
         // after every second the counter adds 1
         counter++;
 
-        
+
     // Accessing and printing elements
-    for (const string& str : gantt_chart) 
-        cout << str ;       
+    for (const string& str : gantt_chart)
+        cout << str ;
 
 }
 }
