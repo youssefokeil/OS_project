e13a55570fc9fcc3622590527457865e92ef8eac
fkamalo <167641692+fkamalo@users.noreply.github.com>
2025-04-25 11:49:29 +0300

    Update process_class.h

--- process_class.h
+++ process_class.h
@@ -2,7 +2,6 @@
 
 #include <mutex>
 #include <thread>
-
 #include <iostream>
 #include <vector>
 #include <map>
@@ -17,55 +16,54 @@ class Process{
     
 private:
 
-int id; 
-int arrival_time;
-int burst_time;
-int waiting_time;
-int turnaround_time;
-int start_time;
-int finish_time;
-int remaining_time; // for scheduling algorithms with preemption (SJF_RR)
-int priority; // for priority scheduling
-
+    int id; 
+    int arrival_time;
+    int burst_time;
+    int waiting_time;
+    int turnaround_time;
+    int start_time;
+    int finish_time;
+    int remaining_time; // for scheduling algorithms with preemption (SJF_RR)
+    int priority; // for priority scheduling
+    int rr_id;
 
 
 public:    
 
 
-// Constructor
-Process();
+    // Constructor
+    Process();
 
-// Constructor for FCFS, RR and SJF scheduling
-Process(int id, int arrival_time, int burst_time);
+    // Constructor for priority scheduling
+    Process(int id,int arrival_time,int burst_time,int priority);
 
+    // setters
+    void setFinishTime(int finish_time);
+    void setBurstTime(int burst_time);
+    void setStartTime(int start);
+    void setRemainingTime(int time);
+    void setRrId(int rrId);
 
-// Constructor for priority scheduling
-Process(int id,int arrival_time,int burst_time,int priority);
+    // getters
+    int getArrivalTime() const;
+    int getBurstTime() const;
+    int getFinishTime() const;
+    int getWaitingTime() const;
+    int getTurnaroundTime() const;
+    int getID() const;
+    int getRemainingTime() const ;
+    int getRrId() const;
 
-// setters
-void setFinishTime(int finish_time);
-void setBurstTime(int burst_time);
-void setStartTime(int start);
-void setRemainingTime(int time);
-// Priority scheduling
-void set_priority(int priority);
+    // Priority scheduling
+    int get_priority() const;
 
-// calculation
-int calc_waiting_time();
-int calc_turnaround_time();
-void updateRemainingTime(int timeUsed);
+    // calculation
+    int calc_waiting_time();
+    int calc_turnaround_time();
+    void updateRemainingTime(int timeUsed);
 
-// getters
-int getArrivalTime() const;
-int getBurstTime() const;
-int getFinishTime() const;
-int getWaitingTime() const;
-int getTurnaroundTime() const;
-int getID() const;
-int getRemainingTime() const ;
-// Priority scheduling
-int get_priority() const;
-
+    // Priority scheduling
+    int get_priority() const;
 
     
 };
@@ -77,10 +75,3 @@ struct Output {
     vector<string> ganttChart;
 
 };
-
-/*
-bool sortByArrivalTime(const Process &a, const Process &b);
-
-bool sortByPriority(const Process &a, const Process &b);
-
-void printOutput(const Output& output, const string& title);*/
