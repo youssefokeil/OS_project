6adf796190969feb1ad41c8d3fddf646ccb97599
abdelrahmanFathy <167958380+AbdelrahmanFathy182@users.noreply.github.com>
2025-04-25 15:14:04 +0300

    qt

---
+++ QT Source projecy/untitled/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,275 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.8.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QStackedWidget *stackedWidget;
+    QWidget *page_3;
+    QTableWidget *tableWidget_2;
+    QComboBox *comboBox;
+    QLabel *label;
+    QSpinBox *spinBox;
+    QPushButton *pushButton;
+    QLabel *label_6;
+    QPushButton *pushButton_4;
+    QLabel *label_12;
+    QSpinBox *spinBox_2;
+    QWidget *page_4;
+    QLabel *label_2;
+    QLabel *label_4;
+    QLabel *label_5;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QSpinBox *spinBox_3;
+    QSpinBox *spinBox_4;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_6;
+    QLabel *label_3;
+    QLabel *label_7;
+    QLabel *label_8;
+    QLabel *label_9;
+    QLabel *label_10;
+    QLabel *label_11;
+    QTableWidget *tableWidget;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(800, 600);
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Fixed, QSizePolicy::Policy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        QFont font;
+        font.setPointSize(12);
+        MainWindow->setFont(font);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        sizePolicy.setHeightForWidth(centralwidget->sizePolicy().hasHeightForWidth());
+        centralwidget->setSizePolicy(sizePolicy);
+        stackedWidget = new QStackedWidget(centralwidget);
+        stackedWidget->setObjectName("stackedWidget");
+        stackedWidget->setGeometry(QRect(0, 0, 800, 600));
+        stackedWidget->setFont(font);
+        page_3 = new QWidget();
+        page_3->setObjectName("page_3");
+        tableWidget_2 = new QTableWidget(page_3);
+        if (tableWidget_2->columnCount() < 3)
+            tableWidget_2->setColumnCount(3);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        tableWidget_2->setHorizontalHeaderItem(0, __qtablewidgetitem);
+        QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
+        tableWidget_2->setHorizontalHeaderItem(1, __qtablewidgetitem1);
+        QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
+        tableWidget_2->setHorizontalHeaderItem(2, __qtablewidgetitem2);
+        tableWidget_2->setObjectName("tableWidget_2");
+        tableWidget_2->setGeometry(QRect(410, 170, 331, 401));
+        sizePolicy.setHeightForWidth(tableWidget_2->sizePolicy().hasHeightForWidth());
+        tableWidget_2->setSizePolicy(sizePolicy);
+        tableWidget_2->setFont(font);
+        tableWidget_2->setColumnCount(3);
+        comboBox = new QComboBox(page_3);
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->setObjectName("comboBox");
+        comboBox->setGeometry(QRect(30, 170, 260, 30));
+        sizePolicy.setHeightForWidth(comboBox->sizePolicy().hasHeightForWidth());
+        comboBox->setSizePolicy(sizePolicy);
+        comboBox->setFont(font);
+        label = new QLabel(page_3);
+        label->setObjectName("label");
+        label->setGeometry(QRect(35, 220, 120, 30));
+        spinBox = new QSpinBox(page_3);
+        spinBox->setObjectName("spinBox");
+        spinBox->setGeometry(QRect(200, 220, 50, 30));
+        spinBox->setValue(1);
+        pushButton = new QPushButton(page_3);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(210, 350, 80, 30));
+        label_6 = new QLabel(page_3);
+        label_6->setObjectName("label_6");
+        label_6->setGeometry(QRect(250, 70, 281, 71));
+        QFont font1;
+        font1.setPointSize(32);
+        label_6->setFont(font1);
+        pushButton_4 = new QPushButton(page_3);
+        pushButton_4->setObjectName("pushButton_4");
+        pushButton_4->setGeometry(QRect(30, 350, 80, 30));
+        label_12 = new QLabel(page_3);
+        label_12->setObjectName("label_12");
+        label_12->setGeometry(QRect(35, 270, 120, 30));
+        spinBox_2 = new QSpinBox(page_3);
+        spinBox_2->setObjectName("spinBox_2");
+        spinBox_2->setGeometry(QRect(200, 270, 50, 30));
+        spinBox_2->setValue(1);
+        stackedWidget->addWidget(page_3);
+        page_4 = new QWidget();
+        page_4->setObjectName("page_4");
+        label_2 = new QLabel(page_4);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(50, 29, 181, 31));
+        QFont font2;
+        font2.setPointSize(16);
+        font2.setBold(true);
+        label_2->setFont(font2);
+        label_4 = new QLabel(page_4);
+        label_4->setObjectName("label_4");
+        label_4->setGeometry(QRect(50, 80, 101, 20));
+        label_5 = new QLabel(page_4);
+        label_5->setObjectName("label_5");
+        label_5->setGeometry(QRect(50, 120, 70, 20));
+        pushButton_2 = new QPushButton(page_4);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(220, 160, 91, 29));
+        pushButton_3 = new QPushButton(page_4);
+        pushButton_3->setObjectName("pushButton_3");
+        pushButton_3->setGeometry(QRect(50, 160, 83, 29));
+        spinBox_3 = new QSpinBox(page_4);
+        spinBox_3->setObjectName("spinBox_3");
+        spinBox_3->setGeometry(QRect(220, 80, 61, 29));
+        spinBox_4 = new QSpinBox(page_4);
+        spinBox_4->setObjectName("spinBox_4");
+        spinBox_4->setGeometry(QRect(220, 120, 61, 29));
+        horizontalLayoutWidget = new QWidget(page_4);
+        horizontalLayoutWidget->setObjectName("horizontalLayoutWidget");
+        horizontalLayoutWidget->setGeometry(QRect(10, 510, 781, 61));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setObjectName("horizontalLayout");
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton_5 = new QPushButton(page_4);
+        pushButton_5->setObjectName("pushButton_5");
+        pushButton_5->setGeometry(QRect(400, 50, 121, 41));
+        QFont font3;
+        font3.setPointSize(18);
+        pushButton_5->setFont(font3);
+        pushButton_6 = new QPushButton(page_4);
+        pushButton_6->setObjectName("pushButton_6");
+        pushButton_6->setGeometry(QRect(400, 120, 121, 41));
+        pushButton_6->setFont(font3);
+        label_3 = new QLabel(page_4);
+        label_3->setObjectName("label_3");
+        label_3->setGeometry(QRect(30, 270, 71, 31));
+        QFont font4;
+        font4.setPointSize(16);
+        label_3->setFont(font4);
+        label_7 = new QLabel(page_4);
+        label_7->setObjectName("label_7");
+        label_7->setGeometry(QRect(150, 270, 91, 31));
+        label_7->setFont(font4);
+        label_7->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        label_8 = new QLabel(page_4);
+        label_8->setObjectName("label_8");
+        label_8->setGeometry(QRect(20, 420, 261, 41));
+        label_8->setFont(font4);
+        label_9 = new QLabel(page_4);
+        label_9->setObjectName("label_9");
+        label_9->setGeometry(QRect(20, 340, 211, 41));
+        label_9->setFont(font4);
+        label_10 = new QLabel(page_4);
+        label_10->setObjectName("label_10");
+        label_10->setGeometry(QRect(230, 340, 151, 31));
+        label_10->setFont(font4);
+        label_10->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        label_11 = new QLabel(page_4);
+        label_11->setObjectName("label_11");
+        label_11->setGeometry(QRect(280, 430, 171, 31));
+        label_11->setFont(font4);
+        label_11->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        tableWidget = new QTableWidget(page_4);
+        if (tableWidget->columnCount() < 1)
+            tableWidget->setColumnCount(1);
+        QTableWidgetItem *__qtablewidgetitem3 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem3);
+        tableWidget->setObjectName("tableWidget");
+        tableWidget->setGeometry(QRect(640, 40, 131, 441));
+        stackedWidget->addWidget(page_4);
+        MainWindow->setCentralWidget(centralwidget);
+
+        retranslateUi(MainWindow);
+
+        stackedWidget->setCurrentIndex(0);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        QTableWidgetItem *___qtablewidgetitem = tableWidget_2->horizontalHeaderItem(0);
+        ___qtablewidgetitem->setText(QCoreApplication::translate("MainWindow", "Arrival time", nullptr));
+        QTableWidgetItem *___qtablewidgetitem1 = tableWidget_2->horizontalHeaderItem(1);
+        ___qtablewidgetitem1->setText(QCoreApplication::translate("MainWindow", "Burst time", nullptr));
+        QTableWidgetItem *___qtablewidgetitem2 = tableWidget_2->horizontalHeaderItem(2);
+        ___qtablewidgetitem2->setText(QCoreApplication::translate("MainWindow", "Priority", nullptr));
+        comboBox->setItemText(0, QCoreApplication::translate("MainWindow", "FCFS", nullptr));
+        comboBox->setItemText(1, QCoreApplication::translate("MainWindow", "Preemptive SJF", nullptr));
+        comboBox->setItemText(2, QCoreApplication::translate("MainWindow", "Non Preemptive SJF", nullptr));
+        comboBox->setItemText(3, QCoreApplication::translate("MainWindow", "Preemptive Priority", nullptr));
+        comboBox->setItemText(4, QCoreApplication::translate("MainWindow", "Non Preemptive Priority", nullptr));
+        comboBox->setItemText(5, QCoreApplication::translate("MainWindow", "Round Robin", nullptr));
+
+        label->setText(QCoreApplication::translate("MainWindow", "No of processes", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Visualize", nullptr));
+        label_6->setText(QCoreApplication::translate("MainWindow", "CPU Scheduler", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "Exit", nullptr));
+        label_12->setText(QCoreApplication::translate("MainWindow", "Quantum", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Add new process", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "Burst time", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "Priority", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "ADD", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "Close", nullptr));
+        pushButton_5->setText(QCoreApplication::translate("MainWindow", "Start", nullptr));
+        pushButton_6->setText(QCoreApplication::translate("MainWindow", "Stop", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Time:", nullptr));
+        label_7->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        label_8->setText(QCoreApplication::translate("MainWindow", "Avg Turnaround time:", nullptr));
+        label_9->setText(QCoreApplication::translate("MainWindow", "Avg waiting time:", nullptr));
+        label_10->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        label_11->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        QTableWidgetItem *___qtablewidgetitem3 = tableWidget->horizontalHeaderItem(0);
+        ___qtablewidgetitem3->setText(QCoreApplication::translate("MainWindow", "Burst time", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ QT Source projecy/untitled/mainwindow.cpp
@@ -0,0 +1,309 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QLabel>
+#include "process_class.h"
+#include "CompareStructs.h"
+#include "Scheduler.h"
+#include"process_class.h"
+#include <limits>
+#include "ui_mainwindow.h"
+#include<qtimer.h>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QDebug>
+#include <QMessageBox>
+
+//  min heaps initialization, one sorted by arrival time, and one sorted by burst time.
+priority_queue<Process, std::vector<Process>, CompareByArrivalTime> ArrivalBuffer;
+vector<string> gantt_chart;
+int n;
+Scheduler myScheduler=Scheduler("FCFS", false);
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->tableWidget_2->setRowCount(1);
+    schedulerTimer = new QTimer(this);
+    connect(schedulerTimer, &QTimer::timeout, this, &MainWindow::processSchedulerStep);
+
+    ui->label_12->hide();
+    ui->spinBox_2->hide();
+    ui->spinBox_2->setMinimum(1);
+    ui->spinBox->setMinimum(1);
+
+    QTableWidgetItem *item = ui->tableWidget_2->item(0, 2);
+    if (!item) {
+        item = new QTableWidgetItem("0");
+        ui->tableWidget_2->setItem(0, 2, item);
+    }
+    item->setFlags(item->flags() & ~Qt::ItemIsEditable);
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+QHBoxLayout* MainWindow::getHorizontalLayout() const {
+    return ui->horizontalLayout;
+}
+QTableWidget* MainWindow::getTableWidget() const{
+    return ui->tableWidget;
+}
+/**
+ * @brief Checks if all cells in a QTableWidget contain valid numbers
+ * @param table The QTableWidget to validate
+ * @return true if ALL cells contain numbers, false otherwise
+ */
+bool validateAllCellsContainNumbers(QTableWidget* table) {
+    if (!table) return false;  // Safety check
+
+    for (int row = 0; row < table->rowCount(); ++row) {
+        for (int col = 0; col < table->columnCount(); ++col) {
+            QTableWidgetItem* item = table->item(row, col);
+
+            // Check if cell exists and isn't empty
+            if (!item || item->text().isEmpty()) {
+                return false;
+            }
+
+            // Try to convert to number
+            bool ok;
+            item->text().toDouble(&ok);  // Works for both ints and floats
+
+            if (!ok) {
+                return false;
+            }
+        }
+    }
+    return true;  // All cells contain numbers
+}
+void MainWindow::on_pushButton_clicked()//visualize button
+{
+    if(validateAllCellsContainNumbers(ui->tableWidget_2)){
+    n=ui->spinBox->value();
+        // input receival from terminal
+        for(int i=0; i<n; i++){
+            int art = 0, bt=0, priority=0;
+
+            art=ui->tableWidget_2->item(i,0)->text().toInt();
+
+            bt=ui->tableWidget_2->item(i,1)->text().toInt();
+
+            ui->tableWidget->setRowCount(n);
+            QTableWidgetItem *item = new QTableWidgetItem(QString::number(bt));
+            ui->tableWidget->setItem(i,0,item);
+
+            if(ui->comboBox->currentText()=="Non Preemptive Priority"||ui->comboBox->currentText()=="Preemptive Priority")
+            priority=ui->tableWidget_2->item(i,2)->text().toInt();
+
+            Process  p(i+1, art, bt, priority);
+            ArrivalBuffer.push(p);
+        }
+
+        ui->stackedWidget->setCurrentIndex(1);
+    }else{
+        QMessageBox::critical(this, "Error", "Please complete all process details in the table");
+    }
+
+}
+
+
+void MainWindow::on_spinBox_valueChanged(int arg1)
+{
+    ui->tableWidget_2->setRowCount(arg1);
+    if(ui->comboBox->currentText()!="Non Preemptive Priority" && ui->comboBox->currentText()!="Preemptive Priority"){
+        if (!ui->tableWidget_2 || 2 < 0 || 2 >= ui->tableWidget_2->columnCount()) {
+            return;
+        }
+
+        // Fill column with zeros and make read-only
+        for (int row = 0; row < ui->tableWidget_2->rowCount(); ++row) {
+            QTableWidgetItem* item = ui->tableWidget_2->item(row, 2);
+            if (!item) {
+                item = new QTableWidgetItem("0");  // Pre-fill with zero
+                ui->tableWidget_2->setItem(row, 2, item);
+            } else {
+                item->setText("0");  // Ensure value is zero
+            }
+            item->setFlags(item->flags() & ~Qt::ItemIsEditable);  // Disable editing
+        }
+    }
+}
+
+
+// Usage:
+ // Assuming you have a layout pointer
+void MainWindow::clearLayout(QLayout* layout) {
+    if (!layout) return;
+
+    QLayoutItem* item;
+    while ((item = layout->takeAt(0)) != nullptr) {
+        if (item->widget()) {
+            delete item->widget(); // Delete the widget
+        }
+        delete item; // Delete the layout item
+    }
+}
+
+
+void MainWindow::on_pushButton_3_clicked()//Close button
+{
+    this->close();
+}
+
+int MainWindow:: getSpinboxValue()const{
+    return ui->spinBox_3->value();
+}
+
+void MainWindow::on_pushButton_2_clicked()//Add button
+{
+
+    int bt = ui->spinBox_3->text().toInt();
+    int priority = ui->spinBox_4->text().toInt();
+    ui->tableWidget->setRowCount(++n);
+    QTableWidgetItem *item = new QTableWidgetItem(QString::number(ui->spinBox_3->value()));
+    ui->tableWidget->setItem(n-1,0,item);
+    ArrivalBuffer.push(Process(n,myScheduler.getcounter(),bt,priority));
+
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    this->close();
+}
+
+
+void MainWindow::on_comboBox_currentIndexChanged(int index)
+{
+
+    if(ui->comboBox->currentText()=="Non Preemptive Priority" || ui->comboBox->currentText()=="Preemptive Priority"){
+    for (int row = 0; row < ui->tableWidget_2->rowCount(); ++row) {
+        QTableWidgetItem* item = ui->tableWidget_2->item(row, 2);
+        if (!item) {
+            item = new QTableWidgetItem();  // Create if doesn't exist
+            ui->tableWidget_2->setItem(row, 2, item);
+        }
+        item->setFlags(item->flags() | Qt::ItemIsEditable);  // Enable editing
+    }}
+
+
+
+    if(ui->comboBox->currentText()=="Round Robin"){
+        ui->label_12->show();
+        ui->spinBox_2->show();
+    }else{
+        ui->label_12->hide();
+        ui->spinBox_2->hide();
+    }
+
+
+
+    switch (index){
+
+    case 0:
+        myScheduler.setScheduler_type("FCFS");
+        myScheduler.setPreemptive(false);
+        break;
+
+    case 1:
+        myScheduler.setScheduler_type("SJF");
+        myScheduler.setPreemptive(true);
+        break;
+
+    case 2:
+        myScheduler.setScheduler_type("SJF");
+        myScheduler.setPreemptive(false);
+        break;
+
+    case 3:
+        myScheduler.setScheduler_type("PRIORITY");
+        myScheduler.setPreemptive(true);
+        break;
+
+    case 4:
+        myScheduler.setScheduler_type("PRIORITY");
+        myScheduler.setPreemptive(false);
+        break;
+
+    case 5:
+        myScheduler.setScheduler_type("ROUNDROBIN");
+        myScheduler.setPreemptive(false);
+        break;
+    }
+
+}
+
+
+
+
+
+double MainWindow::getMinOfColumn(QTableWidget* table, int column) {
+    double minValue = std::numeric_limits<double>::max();
+    bool hasValidValue = false;
+
+    for (int row = 0; row < table->rowCount(); ++row) {
+        if (QTableWidgetItem* item = table->item(row, column)) {
+            bool ok;
+            double value = item->text().toDouble(&ok);
+            if (ok) {
+                if (value < minValue) {
+                    minValue = value;
+                }
+                hasValidValue = true;
+            }
+        }
+    }
+
+    return hasValidValue ? minValue : 0.0;
+};
+
+void MainWindow::on_pushButton_5_clicked()//start
+{
+
+    if (!schedulerTimer->isActive()) {
+                                      // Start processing
+        schedulerTimer->start(1000); // 1000ms = 1 second
+    }
+}
+
+void MainWindow::on_pushButton_6_clicked()//stop
+{
+    if (schedulerTimer->isActive()) {
+        schedulerTimer->stop();
+    }
+}
+int quantum=1;
+int flag=1;
+
+void MainWindow::processSchedulerStep()//event triggered every 1 sec
+{   int q=1;
+    if(ui->comboBox->currentText()=="Round Robin"){
+        q=ui->spinBox_2->value();
+        }
+    //quantum=q;
+    if(flag){
+        quantum=q;
+        flag=0;
+    }
+    ui->tableWidget->setRowCount(n);
+    myScheduler.run_scheduler_once(ArrivalBuffer,this, quantum);
+    quantum--;
+    if(quantum==0)quantum=q;
+    QString str = QString::number(myScheduler.getcounter());
+    ui->label_7->setText(str);
+
+    str=QString::number(myScheduler.getAvgTurnAround()/n,'f',3);
+    ui->label_11->setText(str);
+    str=QString::number(myScheduler.getAvgWaitingTime()/n,'f',3);
+    ui->label_10->setText(str);
+
+
+
+}
+
+
+
