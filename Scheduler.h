b57d40da2c763f7d4c7e93c3caf6ee09eba62496
fkamalo <167641692+fkamalo@users.noreply.github.com>
2025-04-25 11:45:24 +0300

    complete .h file

--- Scheduler.h
+++ Scheduler.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "process_class.h"
-
+#include"CompareStructs.h"
 using namespace std;
 
 class Scheduler {
@@ -12,12 +12,29 @@ private:
     string scheduler_type;
     // process executing flag default is false, default is preemptive
     bool processExecuting = false, preemptive = true;
-    int counter = 0; // doesn't make sense
+    int counter = 0;
     vector<string> gantt_chart;
-
+    double avgTurnAround=0,avgWaitingTime=0;
     priority_queue<Process, std::vector<Process>, DynamicComparator> MainQueue;
 public:
 
     Scheduler(string , bool );
-    void run_scheduler_once(priority_queue<Process, std::vector<Process>, CompareByArrivalTime> &);
+    void run_scheduler_once(priority_queue<Process, std::vector<Process>, CompareByArrivalTime> &,int &q);
+    vector<std::string>& getGanttChart();
+
+    std::priority_queue<Process, std::vector<Process>, DynamicComparator>& getMainQueue();
+
+    int getcounter();
+
+    double getAvgTurnAround() const;
+
+    void setAvgTurnAround(double avgTurnAround);
+
+    double getAvgWaitingTime() const;
+
+    void setAvgWaitingTime(double avgWaitingTime);
+
+    void setScheduler_type(string scheduler_type);
+
+    void setPreemptive(bool preemptive)  ;
 };
